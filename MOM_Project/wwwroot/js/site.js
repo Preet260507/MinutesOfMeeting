5c0f175b1ac780e6b38cc09a448b518a047da290
parmarpreet <parmarpreet265@gmail.com>
2026-03-30 01:04:43 +0530

    js update

--- MOM_Project/wwwroot/js/site.js
+++ MOM_Project/wwwroot/js/site.js
@@ -6,6 +6,14 @@ $(document).ready(function () {
     });
 });
 
+// Double-click protection for forms
+$(document).on('submit', 'form', function () {
+    var $btn = $(this).find('button[type="submit"]');
+    if ($btn.prop('disabled')) return false; // Already submitted
+    $btn.prop('disabled', true);
+    $btn.html('<span class="spinner-border spinner-border-sm me-2"></span>Processing...');
+});
+
 // SweetAlert notification handler
 function showNotification(msgType, message) {
     if (msgType && message) {
